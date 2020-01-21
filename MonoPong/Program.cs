8730f95823edc28d57a4dce46103b41d19bf462a
Jakub Šenkýř <senkyr@spstrutnov.cz>
2020-01-21 11:25:56 +0100

    Holá kostra aplikace

--- MonoPong/Program.cs
+++ MonoPong/Program.cs
@@ -1,22 +1,10 @@
-﻿using System;
-
-namespace MonoPong
+﻿namespace MonoPong
 {
-#if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
-                game.Run();
+            new Hra().Run();
         }
     }
-#endif
 }
