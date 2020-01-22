8da5b5a662f6bcd6dc59f877d363ed593f319d48
Martin Plíhal <plihal@spstrutnov.cz>
2020-01-22 13:38:17 +0100

    Předělání čtverečku na obdélníček a pohyb pálky

--- MonoPong/Palka.cs
+++ MonoPong/Palka.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace MonoPong
 {
-    class Palka : Ctverecek
+    class Palka : Obdelnicek
     {
+        public Palka()
+        {
+
+        }
+
+        public void pohyb(KeyboardState stavKlavesnice, Keys smerNahoru, Keys smerDolu, int Rychlost)
+        {
+            Vector2 smerPohybu = Vector2.Zero;
+
+            if(stavKlavesnice.IsKeyDown(smerNahoru))
+            {
+                smerPohybu -= Vector2.UnitY * Rychlost;
+            }
+            else if(stavKlavesnice.IsKeyDown(smerDolu))
+            {
+                smerPohybu += Vector2.UnitY * Rychlost;
+            }
+        }
     }
 }
