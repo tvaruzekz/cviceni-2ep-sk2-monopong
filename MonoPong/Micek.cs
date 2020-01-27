371c6e2ac53fb4ac2e65461f77c306e87ae1f43f
SprincJ <sprinc@spstrutnov.cz>
2020-01-27 09:11:17 +0100

    Míček už líta, ale jenom rovně?

--- MonoPong/Micek.cs
+++ MonoPong/Micek.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,27 @@ namespace MonoPong
 {
     class Micek : Obdelnicek
     {
+        Random NahodnySmerMickuKamPoleti = new Random();
+        private int LetiTimtoSmerem; 
         public Micek(int velikostx, int velikosty, float x, float y, Color barva, GraphicsDevice zobrazovaciZarizeni) :base (velikostx, velikosty, x, y, barva, zobrazovaciZarizeni)
         {
-
+            LetiTimtoSmerem = NahodnySmerMickuKamPoleti.Next(0, 2);
         }
 
-        public void Letim(Vector2 Rychlost)
+        public void Letim(int Rychlost)
         {
-            Pozice += new Vector2(5,0);
+            if (LetiTimtoSmerem == 2)
+            {
+                Environment.Exit(1);
+            }
+            if (LetiTimtoSmerem == 1)
+            {
+                Pozice += new Vector2(Rychlost, 0);
+            }
+            else
+            {
+                Pozice += new Vector2(-Rychlost, 0);
+            }
         }
     }
 }
